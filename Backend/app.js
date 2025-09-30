67342c00f35810055405f6bddefdb870b378a5f2
Kirteeraj jhala <kirteerajjhala@gmail.com>
2025-09-30 21:36:43 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend url
+    origin: "https://livechatappbykirteeraj.onrender.com", // frontend url
     credentials: true,
   })
 );
